6213317e07d5293203ab6c6839f6935ea454311b
achal1304 <achal20000@gmail.com>
2025-02-10 18:37:43 +0530

    Golang | 841. Keys and Rooms

---
+++ 841/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(canVisitAllRooms([][]int{{1, 3}, {3, 0, 1}, {2}, {0}}))
+}
+
+func canVisitAllRooms(rooms [][]int) bool {
+	if len(rooms) == 0 {
+		return true
+	}
+
+	if len(rooms) == 1 {
+		return true
+	}
+
+	if len(rooms) > 1 && len(rooms[0]) == 0 {
+		return false
+	}
+
+	stack := []int{}
+	for _, ele := range rooms[0] {
+		stack = append(stack, ele)
+	}
+	visited := make(map[int]bool)
+	visited[0] = true
+
+	fmt.Println(len(rooms))
+	for len(stack) > 0 {
+		ele := stack[len(stack)-1]
+		fmt.Println("stack ", stack, "ele ", ele, "visited ", visited)
+		stack = stack[:len(stack)-1]
+
+		if visited[ele] {
+			continue
+		}
+
+		for _, key := range rooms[ele] {
+			stack = append(stack, key)
+		}
+
+		visited[ele] = true
+		if len(visited) == len(rooms) {
+			return true
+		}
+	}
+
+	fmt.Println(len(visited))
+	return false
+}

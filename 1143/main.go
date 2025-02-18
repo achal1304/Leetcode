13f9e130db04da31562141e78a407d0d50df769b
achal1304 <achal20000@gmail.com>
2025-02-18 21:24:31 +0530

    Golang | 1143. Longest Common Subsequence

---
+++ 1143/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestCommonSubsequence("abcba", "abcbcba"))
+}
+
+type Point struct {
+	x, y int
+}
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	dp := make([][]int, len(text1)+1)
+	for i, _ := range dp {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	for i := 1; i < len(text1)+1; i++ {
+		for j := 1; j < len(text2)+1; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], (dp[i][j-1]))
+			}
+		}
+	}
+	for _, ele := range dp {
+		fmt.Println(ele)
+	}
+	return dp[len(text1)][len(text2)]
+}
+
+func max(i, j int) int {
+	if i < j {
+		return j
+	}
+	return i
+}

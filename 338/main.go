68f5f5e67c6d1279dd0911db8c37965587ebaa38
achal1304 <achal20000@gmail.com>
2025-02-22 13:34:41 +0530

    Golang | 338. Counting Bits

---
+++ 338/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(countBits(10))
+}
+
+func countBits(n int) []int {
+	ans := make([]int, n+1)
+
+	for i := 1; i <= n; i++ {
+		ans[i] = ans[i>>1] + (i & 1)
+	}
+
+	return ans
+}

01599141fa6f7c90da25c00703dfc3a83e323def
achal1304 <achal20000@gmail.com>
2025-03-14 17:58:35 +0530

    Golang | 80. Remove Duplicates from Sorted Array II

---
+++ 80/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeDuplicates([]int{1, 1, 1, 2, 2, 2, 4, 4, 4, 5, 5, 5, 6, 7}))
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	} else if len(nums) == 1 {
+		return 1
+	}
+
+	point := 1
+	isSecondOccurance := false
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[point] = nums[i]
+			point++
+			isSecondOccurance = false
+		} else {
+			if !isSecondOccurance {
+				isSecondOccurance = true
+				nums[point] = nums[i]
+				point++
+			}
+		}
+	}
+
+	fmt.Println(nums)
+	return point
+}

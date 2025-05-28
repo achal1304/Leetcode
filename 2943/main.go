068039e1a4c7a3c920c7f86214a48ef2e44b6aab
achal1304 <achal20000@gmail.com>
2025-05-28 09:37:55 +0530

    Golang | 2943. Maximize Area of Square Hole in Grid

---
+++ 2943/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	fmt.Println(maximizeSquareHoleArea(3, 2, []int{3, 2, 4}, []int{2, 3}))
+}
+
+func maximizeSquareHoleArea(n int, m int, hBars []int, vBars []int) int {
+	if len(hBars) == 0 || len(vBars) == 0 {
+		return 1
+	}
+	sort.Ints(hBars)
+	sort.Ints(vBars)
+	hCount := make(map[int]bool)
+	count := 2
+	hCount[2] = true
+	for i := 1; i < len(hBars); i++ {
+		if hBars[i] == hBars[i-1]+1 {
+			count++
+			hCount[count] = true
+		} else {
+			count = 2
+		}
+	}
+
+	vCount := make(map[int]bool)
+	count = 2
+	vCount[2] = true
+	for i := 1; i < len(vBars); i++ {
+		if vBars[i] == vBars[i-1]+1 {
+			count++
+			vCount[count] = true
+		} else {
+			count = 2
+		}
+	}
+	fmt.Println(hCount, vCount)
+	maxCount := 2
+	for k, _ := range hCount {
+		fmt.Println("k", k, maxCount)
+		if vCount[k] {
+			if k > maxCount {
+				maxCount = k
+			}
+		}
+	}
+	return maxCount * maxCount
+}

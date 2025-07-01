8bb8387a3aefe20caac8d88ef3c91b7fa5f0e499
achal1304 <achal20000@gmail.com>
2025-07-01 19:16:20 +0530

    Golang | 98. Validate Binary Search Tree

---
+++ 98/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	return validate(root, math.MinInt64, math.MaxInt64)
+}
+
+func validate(node *TreeNode, min, max int) bool {
+	if node == nil {
+		return true
+	}
+
+	if node.Val <= min || node.Val >= max {
+		return false
+	}
+
+	return validate(node.Left, min, node.Val) &&
+		validate(node.Right, node.Val, max)
+}
